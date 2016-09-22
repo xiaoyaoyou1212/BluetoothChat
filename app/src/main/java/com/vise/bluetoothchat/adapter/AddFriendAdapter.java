1e84384414938faa10aba68f87e44e88d496efc9
xyy <553252628@qq.com>
2016-09-22 22:19:01 +0800

    add list data adapter

---
+++ app/src/main/java/com/vise/bluetoothchat/adapter/AddFriendAdapter.java
@@ -0,0 +1,23 @@
+package com.vise.bluetoothchat.adapter;
+
+import android.content.Context;
+
+import com.vise.bluetoothchat.mode.FriendInfo;
+import com.vise.common_base.adapter.helper.HelperAdapter;
+import com.vise.common_base.adapter.helper.HelperViewHolder;
+
+/**
+ * @Description:
+ * @author: <a href="http://www.xiaoyaoyou1212.com">DAWI</a>
+ * @date: 16/9/22 22:00.
+ */
+public class AddFriendAdapter extends HelperAdapter<FriendInfo> {
+    public AddFriendAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void HelpConvert(HelperViewHolder viewHolder, int position, FriendInfo friendInfo) {
+
+    }
+}
